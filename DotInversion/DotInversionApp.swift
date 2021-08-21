823f1cfa6ebe17be4b2e959c30e3900db6310477
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-21 22:36:48 +0200

    Initial Commit

---
+++ DotInversion/DotInversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotInversionApp.swift
+//  DotInversion
+//
+//  Created by Michele Manniello on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DotInversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
